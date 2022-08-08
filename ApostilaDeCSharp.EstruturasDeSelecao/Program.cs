6761371ce53329e284d8d17eeba726ca61b3ca2c
deyve853 <deyverson853@gmail.com>
2022-08-08 08:50:45 -0300

    atualizado

--- ApostilaDeCSharp.EstruturasDeSelecao/Program.cs
+++ ApostilaDeCSharp.EstruturasDeSelecao/Program.cs
@@ -10,6 +10,44 @@ namespace ApostilaDeCSharp.EstruturasDeSelecao
     {
         static void Main(string[] args)
         {
+            //if - estrutura de seleção simples 
+            double x = 43.5;
+            if (x >= 43.1)
+            {
+                Console.WriteLine("MandaLoriano");
+            }
+
+            //if else - estrutura de seleção composta
+                //Quem define o tipo é o compilador
+            var idade = 43.1;
+            if (idade >= 43.1)
+            {
+                Console.WriteLine("MandaLoriano");
+            }
+            else 
+            {
+                Console.WriteLine("Grande jovem jovem");
+            }
+
+            //if else if - estrutura de seleção composta encadeada
+            if (idade >= 43.1)
+            {
+                Console.WriteLine("MandaLoriano");
+            }
+            else if (idade >= 18 && idade < 43.1)
+            {
+                Console.WriteLine("Grande jovem jovem");
+            }
+            else 
+            {
+                Console.WriteLine("Menino(a)");
+            }
+            //switch - estrutura de seleção múltipla
+            var dia = 25.6;
+            switch () 
+            {
+                
+            }
         }
     }
 }
